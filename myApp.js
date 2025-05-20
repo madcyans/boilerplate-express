2723268bd9523dd254c28a261e350e59ce78741d
Syan <torressyanvale@gmail.com>
2025-05-20 17:55:49 +0800

    up

--- myApp.js
+++ myApp.js
@@ -1,13 +1,7 @@
 let express = require('express');
 let app = express();
 
-// Define a route for GET requests to "/"
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// Start the server and listen on port 3000
-app.listen(PORT, () => console.log(`Node is listening on port ${PORT}...`));
+console.log("Hello, World!");
 
 
 
