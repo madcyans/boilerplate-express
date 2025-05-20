8aa953c38cf96c0eb693df8b96a32539fe84b103
Syan <torressyanvale@gmail.com>
2025-05-20 19:21:23 +0800

    readded

--- myApp.js
+++ myApp.js
@@ -3,13 +3,15 @@ let app = express();
 
 // ********** Your code starts here **********
 
+// This line serves static files from the "public" directory
+app.use('/public', express.static(__dirname + '/public'));
+
+
 // Use the app.get() method to serve "Hello Express" on the root path.
 app.get('/', (req, res) => {
   console.log("Received GET request on '/'");
   res.sendFile(__dirname + '/views/index.html');
 });
-
-app.use('/public', express.static(__dirname + '/public'));
 
 // ********** Your code ends here **********
 
