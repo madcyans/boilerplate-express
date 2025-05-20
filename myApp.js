a863aad946c2ae44775a0a4e79c58d701dfcff81
Syan <torressyanvale@gmail.com>
2025-05-20 18:26:58 +0800

    add serving

--- myApp.js
+++ myApp.js
@@ -3,6 +3,16 @@ let app = express();
 
 console.log("Hello, World!");
 
+// ********** Your code starts here **********
+
+// Use the app.get() method to serve "Hello Express" on the root path.
+app.get('/', (req, res) => {
+  console.log("Received GET request on '/'");
+  res.send("Hello Express");
+});
+
+// ********** Your code ends here **********
+
 
 
 
