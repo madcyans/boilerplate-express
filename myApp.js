84af842f36df0d0f55c75f84029f307b08f77bb3
Syan <torressyanvale@gmail.com>
2025-05-21 05:43:02 +0800

    back

--- myApp.js
+++ myApp.js
@@ -1,35 +1,9 @@
-require('dotenv').config()
+
 
 let express = require('express');
 let app = express();
 
-// ********** Your code starts here **********
-
-// This line serves static files from the "public" directory
-app.use('/public', express.static(__dirname + '/public'));
-
-
-// Use the app.get() method to serve "Hello Express" on the root path.
-app.get('/', (req, res) => {
-  console.log("Received GET request on '/'");
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-// Define a GET route for /json that responds with JSON
-app.get('/json', (req, res) => {
-  console.log("Received GET request on '/json'");
-  console.log("MESSAGE_STYLE:", process.env.MESSAGE_STYLE);
-  // Set the base message
-  let message = "Hello json";
-  // Read the environment variable MESSAGE_STYLE each time the route is hit
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-    message = message.toUpperCase();
-  }
-  
-  // Respond with the JSON object
-  res.json({ "message": message });
-});
-// ********** Your code ends here **********
+console.log("Hello, World!");
 
 
 
